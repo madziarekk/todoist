f85ccf28a8920f7d2ff49ab92e5d1449c66d585f
magdalena-kuczaba <mj.kuczaba@gmail.com>
2012-08-27 18:55:45 +0200

    przeniesienie katalogu

---
+++ todoist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoist::Application.config.secret_token = 'd1b7f59c30b176cb1b18cf737ec6774de291b77867801030d020d0ef04f33a06e8ee4141f79a6d09e704389823f483f849ed75449917cefb8a75d6026ec67a00'
