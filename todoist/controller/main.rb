5424b2d9d5fdf4802305e70d5a9b7fc02ff4f0e9
magdalena-kuczaba <mj.kuczaba@gmail.com>
2012-10-08 19:20:48 +0200

    zajawka strony glownej

--- todoist/controller/main.rb
+++ todoist/controller/main.rb
@@ -11,14 +11,14 @@
 class MainController < Controller
   # the index action is called automatically when no other action is specified
   def index
-    @title = 'Welcome to Ramaze!'
+    @title = 'Todoist'
   end
 
   # the string returned at the end of the function is used as the html body
   # if there is no template for the action. if there is a template, the string
   # is silently ignored
   def notemplate
-    @title = 'Welcome to Ramaze!'
+    @title = 'Todoist - simple app'
     
     return 'There is no \'notemplate.xhtml\' associated with this action.'
   end
